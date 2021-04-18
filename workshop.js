c1130dc521d5b40c8a6298e22a684fe3d95a71c3
mariellelilja <56651394+mariellelilja@users.noreply.github.com>
2021-04-18 14:47:18 +0200

    Update workshop.js

--- workshop.js
+++ workshop.js
@@ -1,7 +1,22 @@
 // Welcome to the workshop! We will fill up this file during live coding :)
 
 // Recap of variables and data types
+
+const number = 1;
+const b = false;
+const text ="this is some text";
+console.log("Here is my number: " + number);
+
 // Arrays - creation, indexing, length, indexOf, includes
+
+const arr = [1,2,3];
+console.log(arr);
+console.log(arr[2]);
+console.log("arr lenght is: "+arr.length);
+
+console.log(arr[arr.lenght]);
+console.log("Which index no does 2 have?  --> arr.indexOf(2): "+arr.indexOf(2));
+console.log(arr.includes(2));
 // Mutation of arrays - push, pop, shift, unshift, reverse
 // Objects - create, mutate, spread
 // Immutable handling of arrays - concat, map, filter, spread
